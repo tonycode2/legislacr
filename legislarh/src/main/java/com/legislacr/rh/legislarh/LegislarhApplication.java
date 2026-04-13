b8aa92a4a6e6196051f4c0f12ee24a7a8f5f05a7
tonycode2 <velurn123@gmail.com>
2026-04-12 20:02:01 -0600

    feat: adding initial files for the RH part of the project.

---
+++ legislarh/src/main/java/com/legislacr/rh/legislarh/LegislarhApplication.java
@@ -0,0 +1,13 @@
+package com.legislacr.rh.legislarh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegislarhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegislarhApplication.class, args);
+	}
+
+}
---
+++ legislarh/src/test/java/com/legislacr/rh/legislarh/LegislarhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legislacr.rh.legislarh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegislarhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
